86ceabf32fdb867edb61c5b16e5ebc59624faad9
Benjamin Zaporzan <benzaporzan@gmail.com>
2018-07-10 12:24:13 -0400

    cleaned up core

--- src/eden/core.clj
+++ src/eden/core.clj
@@ -1,4 +1,5 @@
 (ns eden.core
+  (:refer-clojure :exclude [eval])
   (:require
    [eden.state-machine :refer [new-state-machine]]
    [eden.ast :refer [astm]]))
@@ -10,25 +11,45 @@
      :astm (astm *sm)}))
 
 
-(defn evaluate-expression [tokens]
+(defn eval-expression-fn [tokens]
   (let [eden (eden)]
     (eden.ast/evaluate-expression (:astm eden) tokens)))
 
 
-(defn parse [tokens]
+;; (eval-expression-fn '[4 * 2])
+
+
+(defmacro eval-expression
+  [& tokens]
+  `(eval-expression-fn (vec (quote ~tokens))))
+
+
+;; (eval-expression 2 + 2 * 4)
+
+
+(defn parse-fn [tokens]
   (let [eden (eden)]
     (eden.ast/parse (:astm eden) tokens)))
 
 
-(defn evaluate [tokens]
+(defmacro parse [& tokens]
+  `(parse-fn (vec (quote ~tokens))))
+
+
+;; (parse 2 + 2)
+
+
+(defn eval-fn [tokens]
   (let [eden (eden)]
     (eden.ast/evaluate (:astm eden) tokens)))
 
 
-;;(evaluate-expression '[ten])
-;;(evaluate-expression '[10 != 12 and "Yes" or "No"])
+(defmacro eval [& tokens]
+  `(eval-fn (vec (quote ~tokens))))
 
 
-;;(evaluate '[print "Hello World"])
+;;(eval print "Hello World!")
 
-;;(parse '[print "Hello World" 2 + 2])
+
+#_(eval-expression
+   10 != 12 and "Yes" or "No")
